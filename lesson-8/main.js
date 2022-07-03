2dec30c5d67a2b54fe964409a16f40d46f5879ad
JuliaShurmei <ivanovaulia428@gmail.com>
2022-07-03 07:35:57 +0300

    Homework 8

---
+++ lesson-8/main.js
@@ -0,0 +1,47 @@
+function Animal(name) {
+    var foodAmount = 50;
+    var self = this;
+
+    function formatFoodAmount() {
+        return foodAmount + ' гр.';
+    }
+
+    this.dailyNorm = function (amount) {
+        if (!arguments.length) return formatFoodAmount();
+
+        if (amount < 30 || amount > 100) {
+            return 'Недопустимое количество корма.';
+        }
+
+        foodAmount = amount;
+
+    };
+
+    this.name = name;
+
+    this.feed = function () {
+        console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
+
+    };
+
+}
+
+function Cat(name) {
+    Animal.apply(this, arguments);
+
+    var animalFeed = this.feed;
+    this.feed = function () {
+        animalFeed();
+        console.log('Кот доволен ^_^');
+        return this;
+    };
+    this.stroke = function () {
+        console.log('Гладим кота.');
+        return this;
+    };
+}
+
+var barsik = new Cat('Барсик');
+console.log(barsik.dailyNorm(75));
+barsik.feed();
+barsik.stroke().feed();
